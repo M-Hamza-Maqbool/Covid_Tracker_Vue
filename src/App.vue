2df95d8148044d74e5f68c93f0b36d74c36352cc
unknown <hamza70fsd@gmail.com>
2021-09-28 05:07:03 -0700

    its first commit just styling and making interface

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Header />
+  <div class="container">
+  <router-view/>
+    </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  }
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+ 
+  <header class="    text-center bg-blue-800 p-4  ">
+    
+    <section class="max-w-screen-lg mx-auto  flex justify-center pb-5 pt-5">
+             <img class="rounded-3xl shadow-2xl center" width="300" height="150" src="../assets/img/images.jpg">
+     </section>
+             <div class=" text-white text-3xl shadow-2xl md:text-5xl">
+                   Covid-19 Tracker App 
+             </div>
+
+ </header>
+</template>
+<script>
+export default {
+  name: 'Header',
+}
+</script>
